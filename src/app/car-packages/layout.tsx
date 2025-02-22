5e2eee34d0d4dcfa464262cdc0ffb88925e25b09
RMRAmadushanka <rmramadushanka@gmail.com>
2025-02-22 17:07:25 +0530

    added

--- src/app/car-packages/layout.tsx
+++ src/app/car-packages/layout.tsx
@@ -1,22 +1,22 @@
 "use client";
+
 import ListingImageGallery from "@/components/listing-image-gallery/ListingImageGallery";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import React, { ReactNode } from "react";
 import MobileFooterSticky from "../../components/MobileFooterSticky";
 import { imageGallery as listingCarImageGallery } from "./listing-car-detail/constant";
 
-const DetailLayout = ({ children }: { children: ReactNode }) => {
+const DetailtLayout = ({ children }: { children: ReactNode }) => {
   const router = useRouter();
   const thisPathname = usePathname();
   const searchParams = useSearchParams();
   const modal = searchParams?.get("modal");
 
   const handleCloseModalImageGallery = () => {
-    const params = new URLSearchParams(searchParams.toString());
+    const params = new URLSearchParams(searchParams.toString()); // Use searchParams directly
     params.delete("modal");
 
-    const newUrl = params.toString() ? `${thisPathname}?${params.toString()}` : thisPathname;
-    router.push(newUrl);
+    router.push(`${thisPathname}?${params.toString()}`);
   };
 
   return (
@@ -26,11 +26,13 @@ const DetailLayout = ({ children }: { children: ReactNode }) => {
         onClose={handleCloseModalImageGallery}
         images={listingCarImageGallery}
       />
+
       <div className="container ListingDetailPage__content">{children}</div>
+
       {/* STICKY FOOTER MOBILE */}
       <MobileFooterSticky />
     </div>
   );
 };
 
-export default DetailLayout;
+export default DetailtLayout;
