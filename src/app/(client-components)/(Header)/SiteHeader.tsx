f7c52ddf554604b1f660d631ecdc8545ca57f3a0
RMRAmadushanka <rmramadushanka@gmail.com>
2025-04-05 21:43:20 +0530

    added changes

--- src/app/(client-components)/(Header)/SiteHeader.tsx
+++ src/app/(client-components)/(Header)/SiteHeader.tsx
@@ -2,9 +2,8 @@
 
 import React, { useRef } from "react";
 
-import Header from "./Header";
-
 import { useThemeMode } from "@/utils/useThemeMode";
+import Header3 from "./Header3";
 
 
 export type SiteHeaders = "Header 1" | "Header 2" | "Header 3";
@@ -16,7 +15,7 @@ const SiteHeader = () => {
   //
   return (
     <>
-      <Header className="shadow-sm dark:border-b dark:border-neutral-700" />
+      <Header3 className="shadow-sm dark:border-b dark:border-neutral-700" />
       <div ref={anchorRef} className="h-1 absolute invisible"></div>
     </>
   );
