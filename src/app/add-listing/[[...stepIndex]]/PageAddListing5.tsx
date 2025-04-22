3a6e4075df59cee3f6d6e5fbebeaebd2040fd376
RMRAmadushanka <rmramadushanka@gmail.com>
2025-04-22 23:37:23 +0530

    added  change

--- src/app/add-listing/[[...stepIndex]]/PageAddListing5.tsx
+++ src/app/add-listing/[[...stepIndex]]/PageAddListing5.tsx
@@ -3,22 +3,20 @@ import ButtonPrimary from "@/shared/ButtonPrimary";
 import Input from "@/shared/Input";
 import Textarea from "@/shared/Textarea";
 
-export interface PageAddListing5Props {}
+export interface PageAddListing5Props { }
 
 const PageAddListing5: FC<PageAddListing5Props> = () => {
-  
+
   return (
     <>
-          <div>
+      <div>
         <h2 className="text-2xl font-semibold">
-        Provide Additional Details About Your Travel Experience
+          Provide Additional Details About Your Travel Experience
         </h2>
         <span className="block mt-2 text-neutral-500 dark:text-neutral-400">
-        Share any additional details about your trip, such as your accommodation preferences, any special requests, and things you love about your travel destination.
-
+          Share any additional details about your trip, such as your accommodation preferences, any special requests, and things you love about your travel destination.
         </span>
       </div>
-
       <Textarea placeholder="..." rows={14} />
     </>
   );
